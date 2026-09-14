420b6f35f2bbe25f3142d5695cf393aa06d73ce9
frank <azic17092000@gmail.com>
2026-09-14 13:03:11 +0900

    feat: develop mobile version

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="title" content="Nestar Demo" />
         <meta name="robots" content="index,follow" />
-        <link rel="icon" type="image.png" href="/img/logo/favicon.svg" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
 
         {/* SEO */}
         <meta
@@ -18,8 +18,8 @@ export default function Document() {
           name="description"
           content={
             "Buy and sell properties anywhere anytime in South Korea. | " +
-            "Покупайте и продавайте недвижимость в Южной Корее в любом месте и в любое время. | " +
-            "대한민국 부동산 거래, 언제 어디서든 자유롭게."
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. | " +
+            "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
           }
         />
       </Head>
